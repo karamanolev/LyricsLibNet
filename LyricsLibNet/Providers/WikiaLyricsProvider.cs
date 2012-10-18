5990efccf6388a4c37096be36d69adf87471a5e1
Ivailo Karamanolev <ivailo@karamanolev.com>
2012-10-17 20:25:35 -0700

    Stub for wikia.com

---
+++ LyricsLibNet/Providers/WikiaLyricsProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LyricsLibNet.Providers
+{
+    class WikiaLyricsProvider : HttpLyricsProviderBase
+    {
+        public override string Name
+        {
+            get { return "wikia.com"; }
+        }
+
+        public override string GetRequestUrl(string artist, string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override LyricsResult ParseResult(string requestArtist, string requestTrackTitle, string result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
